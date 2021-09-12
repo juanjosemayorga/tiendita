4d0177a63127ed9675e455df1a1669a2633bcf1c
Juan Jose Mayorga <juan.mayorga@ceiba.com>
2021-09-12 15:41:19 -0500

    offers card started

--- src/components/Pages/OffersPage/OffersCard/OffersCard.tsx
+++ src/components/Pages/OffersPage/OffersCard/OffersCard.tsx
@@ -1,19 +1,35 @@
 import PropTypes from 'prop-types'
 import './OffersCard.css'
+import LIMON from '../../../../assets/images/Name=Limon.jpg'
 
 export const OffersCard = (props: any) => {
   return (
-    <div>
-      <p>32% dto.</p>
-      <figure>
-        {/* img */}
+    <div className="offers-card-container">
+      <div className="offers-card-container__disccount">
+        <p className="offers-card-container__disccount-label">32% dto.</p>
+      </div>
+      <figure className="offers-card-container__figure-product">
+        <img
+          className="offers-card-container__figure-product-image"
+          src={LIMON}
+          alt="Limón" />
       </figure>
-      <article>
-        <p>$26.82/kg (precio rebajado)</p>
-        <p>$39.9/kg (precio normal)</p>
+      <article className="offers-card-container__prices-container">
+        <p
+          className="offers-card-container__prices-container--low-price">
+          $26.82/kg
+        </p>
+        <p
+          className="offers-card-container__prices-container--normal-price">
+          $39.9/kg
+        </p>
       </article>
-      <p>Limón con semilla</p>
-      <button>Agregar</button>
+      <p className="offers-card-container__description-text">
+        Limón con semilla
+      </p>
+      <button className="offers-card-container__add-button">
+        Agregar
+      </button>
     </div>
   )
 }
